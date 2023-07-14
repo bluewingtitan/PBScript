1ddd5f750d85490297665cd78a6e8686084ecaec
bluewingtitan <thebluewingtitan@gmail.com>
2023-07-14 11:53:26 +0200

    bump version

--- PBScript/PBScript/PbsInfo.cs
+++ PBScript/PBScript/PbsInfo.cs
@@ -9,9 +9,9 @@ public static class PbsInfo
     public static string VersionCodeDetailed = $"{MajorVersion}.{MinorVersion}.{SubMinorVersion} patch_{PatchVersion}";
     
     public const int MajorVersion = 0;
-    public const int MinorVersion = 7;
-    public const int SubMinorVersion = 2;
-    public const string PatchVersion = "220331a";
+    public const int MinorVersion = 8;
+    public const int SubMinorVersion = 0;
+    public const string PatchVersion = "230714a";
 
     /// <summary>
     /// Feel free to print this somewhere in the project you are using PBScript in to give credit.
